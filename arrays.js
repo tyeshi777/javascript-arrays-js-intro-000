3d99faa5ec2411f6922261d43c91b9f273478e93
tenzing yeshi <tyeshi777@yahoo.com>
2018-11-02 20:13:33 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -22,7 +22,7 @@ function destructivelyRemoveElementFromBeginningOfArray(chocolateBars)
   return chocolateBars;
 }
 function removeElementFromBeginningOfArray(chocolateBars) {
-  chocolateBars.slice(1);
+  chocolateBars = chocolateBars.slice(1);
   return chocolateBars;
 }
 function destructivelyRemoveElementFromEndOfArray(chocolateBars) {
